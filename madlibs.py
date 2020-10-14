121dffadd44c92db167e1953f47f5cc3c2f5b3a8
sunniraleigh <sraleigh@smith.edu>
2020-10-14 23:28:57 +0000

    finish lab

--- madlibs.py
+++ madlibs.py
@@ -14,7 +14,6 @@ AWESOMENESS = [
     'smashing', 'lovely',
 ]
 
-
 @app.route('/')
 def start_here():
     """Display homepage."""
@@ -41,6 +40,23 @@ def greet_person():
                            person=player,
                            compliment=compliment)
 
+@app.route('/game')
+def show_madlib_form():
+    answer = request.args.get("play_game")
+    if answer == "no":
+        return render_template("goodbye.html")
+    else:
+        return render_template("game.html")
+
+@app.route('/madlib')
+def show_madlib():
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    person = request.args.get("person")
+    adjective = request.args.get("adjective")
+
+    return render_template("madlib.html", 
+    color=color, noun=noun, person=person, adjective=adjective)
 
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
